411b3dab213304f6a0f868820c2a0e97d2d51cc3
Stuart Leeks <stuart@leeks.net>
2017-07-04 13:41:00 +0100

    Strip back project and add core functionality

--- HtmlRefresh/Program.cs
+++ HtmlRefresh/Program.cs
@@ -1,25 +1,54 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Threading.Tasks;
 
 namespace HtmlRefresh
 {
     public class Program
     {
+        private static string Html;
+
         public static void Main(string[] args)
         {
+            Console.WriteLine("HtmlRefresh Starting...");
+            BuildHtmlFromOptions();
+
+            Console.WriteLine("Start pipeline.");
+
             var host = new WebHostBuilder()
                 .UseKestrel()
-                .UseContentRoot(Directory.GetCurrentDirectory())
-                .UseIISIntegration()
-                .UseStartup<Startup>()
-                .UseApplicationInsights()
+                .Configure(app => app.Run(RenderHtml))
                 .Build();
 
             host.Run();
         }
+
+        private static void BuildHtmlFromOptions()
+        {
+            Console.WriteLine("Options: ");
+
+            var backgroundColour = Environment.GetEnvironmentVariable("HTML_REFRESH_BGCOLOUR") ?? "azure";
+            Console.WriteLine($"\tBgColour: {backgroundColour}");
+
+            var foregroundColour = Environment.GetEnvironmentVariable("HTML_REFRESH_FGCOLOUR") ?? "black";
+            Console.WriteLine($"\tFgColour: {foregroundColour}");
+
+            var fontSize = Environment.GetEnvironmentVariable("HTML_REFRESH_FONTSIZE") ?? "16vw";
+            Console.WriteLine($"\tFontSize: {fontSize}");
+
+            var message = Environment.GetEnvironmentVariable("HTML_REFRESH_MESSAGE") ?? "%HOSTNAME%";
+            var messageExpanded = Environment.ExpandEnvironmentVariables(message);
+            Console.WriteLine($"\tMessage: '{message}' (expanded: '{messageExpanded}')");
+
+            Html = $"<html><head><meta http-equiv=\"refresh\" content=\"1\"></head><body style=\"background-color: {backgroundColour};color: {foregroundColour};font-size: {fontSize};font-family: 'Open Sans',sans-serif; margin:0;padding:20px\">{messageExpanded}</body></head>";
+        }
+
+        private static async Task RenderHtml(HttpContext context)
+        {
+            await context.Response.WriteAsync(Html);
+        }
+
     }
 }
